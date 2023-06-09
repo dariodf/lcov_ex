872aafcb0172a706e0f8a7b03f6d0359e1d5eb4b
dariodf <dario@getsavi.com>
2023-06-10 06:46:23 +0900

    Run umbrella app tests from umbrella build

--- lib/lcov_ex.ex
+++ lib/lcov_ex.ex
@@ -70,6 +70,7 @@ defmodule LcovEx do
     path = "#{output}/lcov.info"
     keep? = opts[:keep] || false
     recursing? = Mix.Task.recursing?()
+    arg_path = opts[:arg_path]
     app = Mix.Project.config()[:app]
     app_lcov_path = File.cwd!() |> Path.relative_to(caller_cwd) |> Path.join(path)
 
@@ -78,7 +79,7 @@ defmodule LcovEx do
         # Using --keep option from umbrella
         log_info("\nCoverage file for #{app} created at #{app_lcov_path}")
 
-      not recursing? && File.cwd!() != caller_cwd ->
+      arg_path && File.cwd!() != caller_cwd ->
         # Using an umbrella app path from umbrella
         log_info("\nCoverage file created at #{app_lcov_path}")
 
